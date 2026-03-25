9f8874c13617a65a809101221a944cd360d83eac
Adrián González Blanco <z3u2thnder@proton.me>
2026-03-26 00:32:29 +0100

    add gameService and gatewayApi to repo"

---
+++ getwayApi/src/main/java/com/proyectointermodular/backend/getwayapi/GetwayApiApplication.java
@@ -0,0 +1,13 @@
+package com.proyectointermodular.backend.getwayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetwayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetwayApiApplication.class, args);
+    }
+
+}
---
+++ getwayApi/src/test/java/com/proyectointermodular/backend/getwayapi/GetwayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectointermodular.backend.getwayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetwayApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
