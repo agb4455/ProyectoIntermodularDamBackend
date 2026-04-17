529d02456f1d16f6b6a09c8ffc80fedd62a104c9
Adrián González Blanco <z3u2thnder@proton.me>
2026-04-17 20:28:53 +0200

    feat: initialize Spring Boot project structure with Maven wrapper

---
+++ db_back/src/test/java/com/tfm/db_back/DbBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfm.db_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
